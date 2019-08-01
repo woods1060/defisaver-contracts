af2dee34082307961775304aab5e835cc6fdfbe5
Nikola Klipa <klipa.nikola@gmail.com>
2019-08-01 15:36:11 +0200

    Add dydx to truffle config

--- truffle.js
+++ truffle.js
@@ -8,25 +8,30 @@ const mnemonic = process.env.ETHEREUM_ACCOUNT_MNEMONIC;
 module.exports = {
   networks: {
     kovan: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, process.env.KOVAN_INFURA_ENDPOINT, 0, 15);
-      },
-      network_id: '42',
-      gasPrice: 3000000000,
-      confirmations: 2
-      },
+        provider: function() {
+            return new HDWalletProvider(mnemonic, process.env.KOVAN_INFURA_ENDPOINT, 0, 15);
+        },
+        network_id: '42',
+        gasPrice: 3000000000,
+        confirmations: 2
+    },
     rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, process.env.RINKEBY_INFURA_ENDPOINT, 0, 10);
-      },
-      network_id: '4',
+        provider: function() {
+            return new HDWalletProvider(mnemonic, process.env.RINKEBY_INFURA_ENDPOINT, 0, 10);
+        },
+        network_id: '4',
     },
     test: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, 'http://127.0.0.1:8545/');
-      },
-      network_id: '*',
+        provider: function() {
+            return new HDWalletProvider(mnemonic, 'http://127.0.0.1:8545/');
+        },
+        network_id: '*',
     },
+    dydx: {
+        host: "127.0.0.1",     // Localhost (default: none)
+        port: 8445,            // Standard Ethereum port (default: none)
+        network_id: "1001",       // Any network (default: none)
+    }
   },
   compilers: {
     solc: {
