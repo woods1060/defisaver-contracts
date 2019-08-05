1c1c9c56bd54999e640f96b51505d086f3e82777
Nikola Klipa <klipa.nikola@gmail.com>
2019-08-05 12:04:35 +0200

    Initial deposit method for dydx

--- contracts/savings/dydx/DydxProtocol.sol
+++ contracts/savings/dydx/DydxProtocol.sol
@@ -1,12 +1,59 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 import "../ProtocolInterface.sol";
 import "./lib/Actions.sol";
+import "./lib/Account.sol";
+import "./lib/Types.sol";
+import "../../interfaces/ERC20.sol";
+
+contract ISoloMargin {
+    function operate(
+        Account.Info[] memory accounts,
+        Actions.ActionArgs[] memory actions
+    ) public;
+}
 
 contract DydxProtocol is ProtocolInterface {
 
-    function deposit(address _user, uint _amount) public {
+    ISoloMargin public soloMargin;
+    ERC20 public dai;
 
+    constructor(address _soloMargin, address _daiAddress) public {
+        soloMargin = ISoloMargin(_soloMargin);
+        dai = ERC20(_daiAddress);
+        dai.approve(address(soloMargin), uint(-1));
+    }
+
+    function deposit(address _user, uint _amount) public {
+        require(dai.transferFrom(_user, address(this), _amount));
+
+        Account.Info[] memory accounts = new Account.Info[](1);
+        accounts[0] = Account.Info({
+            owner: address(this),
+            number: 0
+        });
+
+        Actions.ActionArgs[] memory actions = new Actions.ActionArgs[](1);
+        Types.AssetAmount memory amount = Types.AssetAmount({
+            sign: true,
+            denomination: Types.AssetDenomination.Wei,
+            ref: Types.AssetReference.Delta,
+            value: _amount
+        });
+
+        actions[0] = Actions.ActionArgs({
+            actionType: Actions.ActionType.Deposit,
+            accountId: 0,
+            amount: amount,
+            primaryMarketId: 1, //dai market id
+            otherAddress: address(this),
+            secondaryMarketId: 0, //not used
+            otherAccountId: 0, //not used
+            data: "" //not used
+        });
+
+        soloMargin.operate(accounts, actions);
     }
 
     function withdraw(address _user, uint _amount) public {
