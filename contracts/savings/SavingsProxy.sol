d2d07bdcaec6c508ab68475dbf7b0a534a99b375
Nikola Klipa <klipa.nikola@gmail.com>
2019-08-06 18:41:34 +0200

    Add SavingsProxy

--- contracts/savings/SavingsProxy.sol
+++ contracts/savings/SavingsProxy.sol
@@ -1,17 +1,36 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
+import "./ProtocolInterface.sol";
 
 contract SavingsProxy {
 
-    function deposit(uint _amount, uint _protocol) public {
+    enum SavingsProtocol { Compound, Dydx, Fulcrum }
 
+    mapping(bytes32 => address) public protocols;
+
+    constructor(address[] memory _protocols) public {
+        require(_protocols.length == 3);
+
+        protocols[getKeyValue(SavingsProtocol.Compound)] = _protocols[0];
+        protocols[getKeyValue(SavingsProtocol.Dydx)] = _protocols[1];
+        protocols[getKeyValue(SavingsProtocol.Fulcrum)] = _protocols[2];
     }
 
-    function withdraw(uint _amount, uint _protocol) public {
-
+    function deposit(SavingsProtocol _protocol, uint _amount) public {
+        ProtocolInterface(protocols[getKeyValue(_protocol)]).deposit(msg.sender, _amount);
     }
 
-    function swap(uint _from, uint _to) public {
+    function withdraw(SavingsProtocol _protocol, uint _amount) public {
+        ProtocolInterface(protocols[getKeyValue(_protocol)]).withdraw(msg.sender, _amount);
+    }
 
+    function swap(SavingsProtocol _from, SavingsProtocol _to, uint _amount) public {
+        ProtocolInterface(protocols[getKeyValue(_from)]).withdraw(msg.sender, _amount);
+        ProtocolInterface(protocols[getKeyValue(_to)]).deposit(msg.sender, _amount);
+    }
+
+    function getKeyValue(SavingsProtocol _protocol) public view returns(bytes32) {
+        return keccak256(abi.encodePacked(_protocol));
     }
 }
