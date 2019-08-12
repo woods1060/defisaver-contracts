6e63140c86213eb6350f015a6a2da2eeec4c1f05
Nikola Klipa <klipa.nikola@gmail.com>
2019-08-12 13:42:23 +0200

    Rename method

--- contracts/savings/SavingsProxy.sol
+++ contracts/savings/SavingsProxy.sol
@@ -13,25 +13,25 @@ contract SavingsProxy {
         // allow setting only once
         require(protocols[getKeyValue(SavingsProtocol.Compound)] == address(0));
 
-        protocols[getKeyValue(SavingsProtocol.Compound)] = _compoundProtocol;
-        protocols[getKeyValue(SavingsProtocol.Dydx)] = _dydxProtocol;
-        protocols[getKeyValue(SavingsProtocol.Fulcrum)] = _fulcrumProtocol;
+        protocols[getKey(SavingsProtocol.Compound)] = _compoundProtocol;
+        protocols[getKey(SavingsProtocol.Dydx)] = _dydxProtocol;
+        protocols[getKey(SavingsProtocol.Fulcrum)] = _fulcrumProtocol;
     }
 
     function deposit(SavingsProtocol _protocol, uint _amount) public {
-        ProtocolInterface(protocols[getKeyValue(_protocol)]).deposit(msg.sender, _amount);
+        ProtocolInterface(protocols[getKey(_protocol)]).deposit(msg.sender, _amount);
     }
 
     function withdraw(SavingsProtocol _protocol, uint _amount) public {
-        ProtocolInterface(protocols[getKeyValue(_protocol)]).withdraw(msg.sender, _amount);
+        ProtocolInterface(protocols[getKey(_protocol)]).withdraw(msg.sender, _amount);
     }
 
     function swap(SavingsProtocol _from, SavingsProtocol _to, uint _amount) public {
-        ProtocolInterface(protocols[getKeyValue(_from)]).withdraw(msg.sender, _amount);
-        ProtocolInterface(protocols[getKeyValue(_to)]).deposit(msg.sender, _amount);
+        ProtocolInterface(protocols[getKey(_from)]).withdraw(msg.sender, _amount);
+        ProtocolInterface(protocols[getKey(_to)]).deposit(msg.sender, _amount);
     }
 
-    function getKeyValue(SavingsProtocol _protocol) public view returns(bytes32) {
+    function getKey(SavingsProtocol _protocol) public view returns(bytes32) {
         return keccak256(abi.encodePacked(_protocol));
     }
 }
