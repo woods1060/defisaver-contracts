a9635ccfd6a7665dc6728a1e4959aaa6a3e135da
Nikola Klipa <klipa.nikola@gmail.com>
2019-08-12 16:48:31 +0200

    Remove memory from SavingsProxy

--- contracts/savings/SavingsProxy.sol
+++ contracts/savings/SavingsProxy.sol
@@ -8,23 +8,16 @@ import "./dydx/ISoloMargin.sol";
 
 contract SavingsProxy is ConstantAddresses {
 
+    address constant public SAVINGS_COMPOUND_ADDRESS = 0x5bdF0b2FaF59639f41946EE2DeBa90f0e02EB867;
+    address constant public SAVINGS_DYDX_ADDRESS = 0x13903074023f8a269671e12401Cce148eA18784D;
+    address constant public SAVINGS_FULCRUM_ADDRESS = 0x560d427777a206E3152B9b2a998aC52a8660269A;
+
     enum SavingsProtocol { Compound, Dydx, Fulcrum }
-
-    mapping(bytes32 => address) public protocols;
-
-    function addProtocols(address _compoundProtocol, address _dydxProtocol, address _fulcrumProtocol) public {
-        // allow setting only once
-        require(protocols[getKey(SavingsProtocol.Compound)] == address(0));
-
-        protocols[getKey(SavingsProtocol.Compound)] = _compoundProtocol;
-        protocols[getKey(SavingsProtocol.Dydx)] = _dydxProtocol;
-        protocols[getKey(SavingsProtocol.Fulcrum)] = _fulcrumProtocol;
-    }
 
     function deposit(SavingsProtocol _protocol, uint _amount) public {
         approveDeposit(_protocol, _amount);
 
-        ProtocolInterface(protocols[getKey(_protocol)]).deposit(address(this), _amount);
+        ProtocolInterface(getAddress(_protocol)).deposit(address(this), _amount);
 
         endAction(_protocol);
     }
@@ -32,7 +25,7 @@ contract SavingsProxy is ConstantAddresses {
     function withdraw(SavingsProtocol _protocol, uint _amount) public {
         approveWithdraw(_protocol, _amount);
 
-        ProtocolInterface(protocols[getKey(_protocol)]).withdraw(address(this), _amount);
+        ProtocolInterface(getAddress(_protocol)).withdraw(address(this), _amount);
 
         endAction(_protocol);
     }
@@ -41,15 +34,25 @@ contract SavingsProxy is ConstantAddresses {
         approveWithdraw(_from, _amount);
         approveDeposit(_to, _amount);
 
-        ProtocolInterface(protocols[getKey(_from)]).withdraw(address(this), _amount);
-        ProtocolInterface(protocols[getKey(_to)]).deposit(address(this), _amount);
+        ProtocolInterface(getAddress(_from)).withdraw(address(this), _amount);
+        ProtocolInterface(getAddress(_to)).deposit(address(this), _amount);
 
         endAction(_from);
         endAction(_to);
     }
 
-    function getKey(SavingsProtocol _protocol) public view returns(bytes32) {
-        return keccak256(abi.encodePacked(_protocol));
+    function getAddress(SavingsProtocol _protocol) public view returns(address) {
+        if (_protocol == SavingsProtocol.Compound) {
+            return SAVINGS_COMPOUND_ADDRESS;
+        }
+
+        if (_protocol == SavingsProtocol.Dydx) {
+            return SAVINGS_DYDX_ADDRESS;
+        }
+
+        if (_protocol == SavingsProtocol.Fulcrum) {
+            return SAVINGS_FULCRUM_ADDRESS;
+        }
     }
 
     function endAction(SavingsProtocol _protocol)  internal {
@@ -62,7 +65,7 @@ contract SavingsProxy is ConstantAddresses {
         ERC20(MAKER_DAI_ADDRESS).transferFrom(msg.sender, address(this), _amount);
 
         if (_protocol == SavingsProtocol.Compound || _protocol == SavingsProtocol.Fulcrum) {
-            ERC20(MAKER_DAI_ADDRESS).approve(protocols[getKey(_protocol)], _amount);
+            ERC20(MAKER_DAI_ADDRESS).approve(getAddress(_protocol), _amount);
         }
 
         if (_protocol == SavingsProtocol.Dydx) {
@@ -73,7 +76,7 @@ contract SavingsProxy is ConstantAddresses {
 
     function approveWithdraw(SavingsProtocol _protocol, uint _amount) internal {
         if (_protocol == SavingsProtocol.Compound) {
-            ERC20(CDAI_ADDRESS).approve(protocols[getKey(_protocol)], _amount);
+            ERC20(CDAI_ADDRESS).approve(getAddress(_protocol), _amount);
         }
 
         if (_protocol == SavingsProtocol.Dydx) {
@@ -81,14 +84,14 @@ contract SavingsProxy is ConstantAddresses {
         }
 
         if (_protocol == SavingsProtocol.Fulcrum) {
-            ERC20(IDAI_ADDRESS).approve(protocols[getKey(_protocol)], _amount);
+            ERC20(IDAI_ADDRESS).approve(getAddress(_protocol), _amount);
         }
     }
 
     function setDydxOperator(bool _trusted) internal {
         ISoloMargin.OperatorArg[] memory operatorArgs = new ISoloMargin.OperatorArg[](1);
         operatorArgs[0] = ISoloMargin.OperatorArg({
-            operator: protocols[getKey(SavingsProtocol.Dydx)],
+            operator: getAddress(SavingsProtocol.Dydx),
             trusted: _trusted
         });
 
--- migrations/3_deploy_savings_contracts.js
+++ migrations/3_deploy_savings_contracts.js
@@ -9,18 +9,16 @@ module.exports = function(deployer, network, accounts) {
     let deployAgain = (process.env.DEPLOY_AGAIN === 'true') ? true : false;
 
     deployer.then(async () => {
-        await deployer.deploy(SavingsProxy, {gas: 6000000, overwrite: deployAgain})
-        let savingsProxy = await SavingsProxy.deployed()
+        // await deployer.deploy(SavingsProxy, {gas: 6000000, overwrite: deployAgain})
+        // let savingsProxy = await SavingsProxy.deployed()
 
-        await deployer.deploy(DydxSavingsProtocol, savingsProxy.address, {gas: 6000000, overwrite: deployAgain})
-        let dydxSavingsProtocol = await DydxSavingsProtocol.deployed()
+        // await deployer.deploy(DydxSavingsProtocol, savingsProxy.address, {gas: 6000000, overwrite: deployAgain})
+        // let dydxSavingsProtocol = await DydxSavingsProtocol.deployed()
 
-        await deployer.deploy(CompoundSavingsProtocol, savingsProxy.address, {gas: 6000000, overwrite: deployAgain})
-        let compoundSavingsProtocol = await CompoundSavingsProtocol.deployed()
+        // await deployer.deploy(CompoundSavingsProtocol, savingsProxy.address, {gas: 6000000, overwrite: deployAgain})
+        // let compoundSavingsProtocol = await CompoundSavingsProtocol.deployed()
 
         await deployer.deploy(FulcrumSavingsProtocol, savingsProxy.address, {gas: 6000000, overwrite: deployAgain})
         let fulcrumSavingsProtocol = await FulcrumSavingsProtocol.deployed()
-
-        await savingsProxy.addProtocols(compoundSavingsProtocol.address, dydxSavingsProtocol.address, fulcrumSavingsProtocol.address, {gas: 6000000})
     });
 };
