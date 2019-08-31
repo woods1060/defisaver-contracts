5a5c3382dc49f89392d0a101e9148405935c17a9
Nenad Palinkasevic <nesa993@gmail.com>
2019-08-31 18:03:09 +0200

    Automatic Dai savings

---
+++ migrations/6_deploy_automatic_savings_contracts.js
@@ -0,0 +1,13 @@
+const SavingsManager = artifacts.require("./SavingsManager.sol");
+const sDai = artifacts.require("./sDai.sol");
+
+require('dotenv').config();
+
+module.exports = function(deployer, network, accounts) {
+    let deployAgain = (process.env.DEPLOY_AGAIN === 'true') ? true : false;
+
+    deployer.deploy(SavingsManager, {gas: 6720000, overwrite: deployAgain})
+    .then(() => {
+        return deployer.deploy(sDai, SavingsManager.address, {gas: 6720000, overwrite: deployAgain});
+     })
+};
