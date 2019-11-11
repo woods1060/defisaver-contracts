96cca5958b69bf78a84a3f63487fb5b9d7559d56
Nenad Palinkasevic <nesa993@gmail.com>
2019-11-11 13:57:53 +0100

    Going over subscription contract and writing docs

--- contracts/mcd/automatic/Subscriptions.sol
+++ contracts/mcd/automatic/Subscriptions.sol
@@ -8,7 +8,7 @@ import "../../constants/ConstantAddresses.sol";
 import "../maker/Vat.sol";
 import "../maker/Spotter.sol";
 
-// TODO: better handle if user transfers CDP
+/// @title Handles subscriptions for automatic monitoring
 contract Subscriptions is ISubscriptions, ConstantAddresses {
 
     bytes32 internal constant ETH_ILK = 0x4554482d41000000000000000000000000000000000000000000000000000000;
@@ -45,6 +45,7 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
     event Unsubscribed(address indexed owner, uint cdpId);
     event Updated(address indexed owner, uint cdpId);
 
+    /// @param _saverProxy Address of the MCDSaverProxy contract
     constructor(address _saverProxy) public {
         owner = msg.sender;
 
@@ -54,6 +55,13 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
         minLimits[BAT_ILK] = 1700000000000000000;
     }
 
+    /// @dev Called by the DSProxy contract which owns the CDP
+    /// @notice Adds the users CDP in the list of subscriptions so it can be monitored
+    /// @param _cdpId Id of the CDP
+    /// @param _minRatio Minimum ratio below which repay is triggered
+    /// @param _maxRatio Maximum ratio after which boost is triggered
+    /// @param _optimalBoost Ratio amount which boost should target
+    /// @param _optimalRepay Ratio amount which repay should target
     function subscribe(uint _cdpId, uint128 _minRatio, uint128 _maxRatio, uint128 _optimalBoost, uint128 _optimalRepay) external {
         require(isOwner(msg.sender, _cdpId), "Must be called by Cdp owner");
         require(checkParams(manager.ilks(_cdpId), _minRatio, _maxRatio), "Must be correct params");
@@ -85,16 +93,20 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
         }
     }
 
+    /// @dev Called by the users DSProxy
+    /// @title Owner who subscribed cancels his subscription
     function unsubscribe(uint _cdpId) external {
         require(isOwner(msg.sender, _cdpId), "Must be called by Cdp owner");
 
         _unsubscribe(_cdpId);
     }
 
+    /// @dev Checks if the _owner is the owner of the CDP
     function isOwner(address _owner, uint _cdpId) internal view returns (bool) {
         return getOwner(_cdpId) == _owner;
     }
 
+    /// @dev Checks limit for minimum ratio and if minRatio is bigger than max
     function checkParams(bytes32 _ilk, uint128 _minRatio, uint128 _maxRatio) internal view returns (bool) {
         if (_minRatio < minLimits[_ilk]) {
             return false;
@@ -107,10 +119,13 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
         return true;
     }
 
+    /// @dev Gets CDP ratio, calls MCDSaverProxy for getting the ratio
     function getRatio(uint _cdpId) public view returns (uint) {
         return saverProxy.getRatio(_cdpId, manager.ilks(_cdpId)) / (10 ** 18);
     }
 
+    /// @notice Checks if Boost/Repay could be triggered for the CDP
+    /// @dev Called by MCDMonitor to enforce the min/max check
     function canCall(Method _method, uint _cdpId) public view returns(bool) {
         SubPosition memory subInfo = subscribersPos[_cdpId];
 
@@ -129,6 +144,7 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
         }
     }
 
+    /// @dev Internal method to remove a subscriber from the list
     function _unsubscribe(uint _cdpId) internal {
         require(subscribers.length > 0, "Must have subscribers in the list");
 
@@ -145,10 +161,13 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
         emit Unsubscribed(msg.sender, _cdpId);
     }
 
+    /// @notice Returns an address that owns the CDP
+    /// @param _cdpId Id of the CDP
     function getOwner(uint _cdpId) public view returns(address) {
         return manager.owns(_cdpId);
     }
 
+    /// @dev After the Boost/Repay check if the ratio doesn't trigger another call
     function ratioGoodAfter(Method _method, uint _cdpId) public view returns(bool) {
         SubPosition memory subInfo = subscribersPos[_cdpId];
         CdpHolder memory subscriber = subscribers[subInfo.arrPos];
@@ -162,6 +181,7 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
         }
     }
 
+    /// @notice Helper method for the front to get all the info about the subscribed CDP
     function getSubscribedInfo(uint _cdpId) public view returns(bool, uint128, uint128, uint128, uint128, address, uint coll, uint debt) {
         SubPosition memory subInfo = subscribersPos[_cdpId];
 
@@ -183,6 +203,7 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
         );
     }
 
+    /// @notice Helper method for the front to get the information about the ilk of a CDP
     function getIlkInfo(bytes32 _ilk, uint _cdpId) public view returns(bytes32 ilk, uint art, uint rate, uint spot, uint line, uint dust, uint mat, uint par) {
         // send either ilk or cdpId
         if (_ilk == bytes32(0)) {
@@ -195,6 +216,7 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
         (art, rate, spot, line, dust) = vat.ilks(_ilk);
     }
 
+    /// @notice Helper method to return all the subscribed CDPs
     function getSubscribers() public view returns (CdpHolder[] memory) {
         return subscribers;
     }
@@ -202,22 +224,22 @@ contract Subscriptions is ISubscriptions, ConstantAddresses {
 
     ////////////// ADMIN METHODS ///////////////////
 
+    /// @notice Admin function to change a min. limit for an asset
     function changeMinRatios(bytes32 _ilk, uint _newRatio) public {
         require(msg.sender == owner, "Must be owner");
 
         minLimits[_ilk] = _newRatio;
     }
 
+    /// @notice Admin function to unsubscribe a CDP if it's owner transfered to a different addr
     function unsubscribeIfMoved(uint _cdpId) public {
         require(msg.sender == owner, "Must be owner");
 
         SubPosition storage subInfo = subscribersPos[_cdpId];
 
         if (subInfo.subscribed) {
-
-
             if (getOwner(_cdpId) != subscribers[subInfo.arrPos].owner) {
-
+                _unsubscribe(_cdpId);
             }
         }
 
