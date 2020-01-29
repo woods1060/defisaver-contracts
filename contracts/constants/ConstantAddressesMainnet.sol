2192134cfd43afa033b227cb4a2485a803ae0850
Nikola Klipa <klipa.nikola@gmail.com>
2020-01-29 15:42:26 +0100

    Change address to be checksummed

--- contracts/constants/ConstantAddressesMainnet.sol
+++ contracts/constants/ConstantAddressesMainnet.sol
@@ -32,7 +32,7 @@ contract ConstantAddressesMainnet {
 
     address public constant SAVINGS_LOGGER_ADDRESS = 0x89b3635BD2bAD145C6f92E82C9e83f06D5654984;
 
-    address public constant SAVER_EXCHANGE_ADDRESS = 0x6ec6d98e2af940436348883fafd5646e9cde2446;
+    address public constant SAVER_EXCHANGE_ADDRESS = 0x6eC6D98e2AF940436348883fAFD5646E9cdE2446;
 
     // Kovan addresses, not used on mainnet
     address public constant COMPOUND_DAI_ADDRESS = 0x25a01a05C188DaCBCf1D61Af55D4a5B4021F7eeD;
