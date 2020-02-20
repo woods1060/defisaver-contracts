b7e21e89d192498b7d5255500b34b2a28e93a4b6
Nenad Palinkasevic <nesa993@gmail.com>
2020-02-20 12:34:36 +0100

    Dydx flash loan prototype

--- contracts/flashloan/automatic_close/MCDCloseProxy.sol
+++ contracts/flashloan/automatic_close/MCDCloseProxy.sol
@@ -4,13 +4,13 @@ import "../../mcd/maker/Manager.sol";
 
 contract MCDCloseProxy {
 
-    Manager public constant manager = Manager(MANAGER_ADDRESS);
+//     Manager public constant manager = Manager(MANAGER_ADDRESS);
 
-    function subscribe(uint _cdpId) public {
+//     function subscribe(uint _cdpId) public {
 
-    }
+//     }
 
-    function unsubscribe(uint _cdpId) public {
+//     function unsubscribe(uint _cdpId) public {
 
-    }
+//     }
 }
--- contracts/flashloan/automatic_close/MCDCloseSubscribers.sol
+++ contracts/flashloan/automatic_close/MCDCloseSubscribers.sol
@@ -3,35 +3,35 @@ pragma solidity ^0.5.0;
 
 contract MCDCloseSubscribers {
 
-    struct ClosePosition {
-        uint minProfit;
-        uint maxProfit;
-        uint cdpId;
-    }
+    // struct ClosePosition {
+    //     uint minProfit;
+    //     uint maxProfit;
+    //     uint cdpId;
+    // }
 
-    mapping (uint => ClosePosition) public closePositions;
-    uint[] public subscribedCdps;
+    // mapping (uint => ClosePosition) public closePositions;
+    // uint[] public subscribedCdps;
 
-    function subscribe(uint _cdpId, uint _minProfit, uint _maxProfit) public {
-        require(isOwner(msg.sender, _cdpId), "Must be the owner of the CDP");
+    // function subscribe(uint _cdpId, uint _minProfit, uint _maxProfit) public {
+    //     require(isOwner(msg.sender, _cdpId), "Must be the owner of the CDP");
 
-        closePositions[_cdpId] = ClosePosition({
-            minProfit: _minProfit,
-            maxProfit: _maxProfit,
-            cdpId: _cdpId
-        });
-    }
+    //     closePositions[_cdpId] = ClosePosition({
+    //         minProfit: _minProfit,
+    //         maxProfit: _maxProfit,
+    //         cdpId: _cdpId
+    //     });
+    // }
 
-    function unsubscribe(uint _cdpId) public {
-        require(isOwner(msg.sender, _cdpId), "Must be the owner of the CDP");
-    }
+    // function unsubscribe(uint _cdpId) public {
+    //     require(isOwner(msg.sender, _cdpId), "Must be the owner of the CDP");
+    // }
 
-    function canCall() public returns (bool) {
+    // function canCall() public returns (bool) {
 
-    }
+    // }
 
-    /// @dev Checks if the _owner is the owner of the CDP
-    function isOwner(address _owner, uint _cdpId) internal view returns (bool) {
-        return manager.owns(_cdpId) == _owner;
-    }
+    // /// @dev Checks if the _owner is the owner of the CDP
+    // function isOwner(address _owner, uint _cdpId) internal view returns (bool) {
+    //     return manager.owns(_cdpId) == _owner;
+    // }
 }
