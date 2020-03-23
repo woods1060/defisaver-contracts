6df320408a62b37137756595d4b48556f4cb9167
Nenad Palinkasevic <nesa993@gmail.com>
2020-03-23 13:15:02 +0100

    Finishing compounds basic proxy

--- contracts/compound/CompoundBasicProxy.sol
+++ contracts/compound/CompoundBasicProxy.sol
@@ -5,16 +5,25 @@ import "../interfaces/ERC20.sol";
 
 contract CEtherInterface {
     function mint() external payable;
+    function repayBorrow() external payable;
+}
+
+contract ComptrollerInterface {
+    function enterMarkets(address[] calldata cTokens) external returns (uint[] memory);
+    function exitMarket(address cToken) external returns (uint);
 }
 
 contract CompoundBasicProxy {
 
     address public constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public constant COMPTROLLER = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
 
     /// @dev User needs to approve the DSProxy to pull the _tokenAddr tokens
-    function deposit(address _tokenAddr, address _cTokenAddr, uint _amount) external payable {
-        if (_tokenAddr != ETH_ADDRESS) {
-            ERC20(_tokenAddr).approve(_cTokenAddr, uint(-1));
+    function deposit(address _tokenAddr, address _cTokenAddr, uint _amount, bool _inMarket) external payable {
+        approveCToken(_tokenAddr, _cTokenAddr);
+
+        if (!_inMarket) {
+            enterMarket(_cTokenAddr);
         }
 
         if (_tokenAddr != ETH_ADDRESS) {
@@ -42,7 +51,11 @@ contract CompoundBasicProxy {
 
     }
 
-    function borrow(address _tokenAddr, address _cTokenAddr, uint _amount) external {
+    function borrow(address _tokenAddr, address _cTokenAddr, uint _amount, bool _inMarket) external {
+        if (!_inMarket) {
+            enterMarket(_cTokenAddr);
+        }
+
         require(CTokenInterface(_cTokenAddr).borrow(_amount) == 0);
 
         // withdraw funds to msg.sender
@@ -53,12 +66,38 @@ contract CompoundBasicProxy {
         }
     }
 
+    /// @dev User needs to approve the DSProxy to pull the _tokenAddr tokens
+    function payback(address _tokenAddr, address _cTokenAddr, uint _amount) external payable {
+        approveCToken(_tokenAddr, _cTokenAddr);
 
-    function payback(address _tokenAddr, address _cTokenAddr, uint _amount) external {
-
+        if (_tokenAddr != ETH_ADDRESS) {
+            require(CTokenInterface(_cTokenAddr).repayBorrow(_amount) == 0);
+        } else {
+            CEtherInterface(_cTokenAddr).repayBorrow.value(msg.value)();
+            msg.sender.transfer(address(this).balance); // compound will send back the extra eth
+        }
     }
 
-    function withdrawCTokens() external {
+    function withdrawTokens(address _tokenAddr) external {
+        if (_tokenAddr != ETH_ADDRESS) {
+            ERC20(_tokenAddr).transfer(msg.sender, ERC20(_tokenAddr).balanceOf(address(this)));
+        } else {
+            msg.sender.transfer(address(this).balance);
+        }
+    }
 
+    function enterMarket(address _cTokenAddr) public {
+        address[] memory markets = new address[](1);
+        ComptrollerInterface(COMPTROLLER).enterMarkets(markets);
+    }
+
+    function exitMarket(address _cTokenAddr) external {
+        ComptrollerInterface(COMPTROLLER).exitMarket(_cTokenAddr);
+    }
+
+    function approveCToken(address _tokenAddr, address _cTokenAddr) internal {
+        if (_tokenAddr != ETH_ADDRESS) {
+            ERC20(_tokenAddr).approve(_cTokenAddr, uint(-1));
+        }
     }
 }
