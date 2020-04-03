51d56bc39a110ec08a8d24507ed6b20f96a08a6e
Nenad Palinkasevic <nesa993@gmail.com>
2020-04-03 10:59:29 +0200

    Clean up compound saver for flash loans

--- contracts/compound/flashloan/CompoundSaverFlashLoan.sol
+++ contracts/compound/flashloan/CompoundSaverFlashLoan.sol
@@ -2,11 +2,13 @@ pragma solidity ^0.5.0;
 
 import "../../flashloan/aave/FlashLoanReceiverBase.sol";
 import "../../interfaces/DSProxyInterface.sol";
+import "../../interfaces/ERC20.sol";
 
 contract CompoundSaverFlashLoan is FlashLoanReceiverBase {
     ILendingPoolAddressesProvider public LENDING_POOL_ADDRESS_PROVIDER = ILendingPoolAddressesProvider(0x24a42fD28C976A61Df5D00D0599C34c4f90748c8);
 
     address payable public constant COMPOUND_SAVER_FLASH_PROXY = 0x86E132932566fb7030eeF19B997C8797De13CFBD;
+    address public constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     constructor()
         FlashLoanReceiverBase(LENDING_POOL_ADDRESS_PROVIDER)
@@ -19,11 +21,20 @@ contract CompoundSaverFlashLoan is FlashLoanReceiverBase {
         uint256 _fee,
         bytes calldata _params)
     external {
+        // Format the call data for DSProxy
+        (bytes memory proxyData, address payable proxyAddr) = packFunctionCall(_amount, _fee, _params);
 
-        //check the contract has the specified balance
-        require(_amount <= getBalanceInternal(address(this), _reserve),
-            "Invalid balance for the contract");
+        // Send Flash loan amount to DSProxy
+        sendLoanToProxy(proxyAddr, _reserve, _amount);
 
+        // Execute the DSProxy call
+        DSProxyInterface(proxyAddr).execute(COMPOUND_SAVER_FLASH_PROXY, proxyData);
+
+        // Repay the loan with the money DSProxy sent back
+        transferFundsBackToPoolInternal(_reserve, _amount.add(_fee));
+    }
+
+    function packFunctionCall(uint _amount, uint _fee, bytes memory _params) internal returns (bytes memory proxyData, address payable) {
         (
             uint[5] memory data, // amount, minPrice, exchangeType, gasCost, 0xPrice
             address[3] memory addrData, // cCollAddress, cBorrowAddress, exchangeAddress
@@ -33,24 +44,24 @@ contract CompoundSaverFlashLoan is FlashLoanReceiverBase {
         )
         = abi.decode(_params, (uint256[5],address[3],bytes,bool,address));
 
-        bytes memory proxyData;
+        uint[2] memory flashLoanData = [_amount, _fee];
 
         if (isRepay) {
-            proxyData = abi.encodeWithSignature("flashRepay(uint256[5],address[3],bytes)", data, addrData, callData);
+            proxyData = abi.encodeWithSignature("flashRepay(uint256[5],address[3],bytes,uint256[2])", data, addrData, callData, flashLoanData);
         } else {
-            proxyData = abi.encodeWithSignature("flashBoost(uint256[5],address[3],bytes)", data, addrData, callData);
+            proxyData = abi.encodeWithSignature("flashBoost(uint256[5],address[3],bytes,uint256[2])", data, addrData, callData, flashLoanData);
         }
 
-        DSProxyInterface(proxyAddr).execute(COMPOUND_SAVER_FLASH_PROXY, proxyData);
+        return (proxyData, proxyAddr);
+    }
 
-        transferFundsBackToPoolInternal(_reserve, _amount.add(_fee));
-
-        // if there is some eth left (0x fee), return it to user
-        if (address(this).balance > 0) {
-            tx.origin.transfer(address(this).balance);
+    function sendLoanToProxy(address payable _proxy, address _reserve, uint _amount) internal {
+        if (_reserve == ETH_ADDRESS) {
+            _proxy.transfer(_amount);
+        } else {
+            ERC20(_reserve).transfer(_proxy, _amount);
         }
     }
 
     function() external payable {}
-
 }
--- contracts/compound/flashloan/CompoundSaverFlashProxy.sol
+++ contracts/compound/flashloan/CompoundSaverFlashProxy.sol
@@ -3,24 +3,31 @@ pragma solidity ^0.5.0;
 import "../CompoundSaverProxy.sol";
 import "../../mcd/Discount.sol";
 
-
 contract CompoundFlashSaverProxy is CompoundSaverProxy {
 
     // TODO: don't use msg.value
     function flashRepay(
         uint[5] memory _data, // amount, minPrice, exchangeType, gasCost, 0xPrice
         address[3] memory _addrData, // cCollAddress, cBorrowAddress, exchangeAddress
-        bytes memory _callData
+        bytes memory _callData,
+        uint[2] memory _flashLoanData // amount, fee
     ) public payable {
-        // repay(_data, _addrData, _callData);
+        // draw max coll
+        // swap max coll + loanAmount
+        // payback debt
+        // draw collateral for loanAmount + loanFee
     }
 
      function flashBoost(
         uint[5] memory _data, // amount, minPrice, exchangeType, gasCost, 0xPrice
         address[3] memory _addrData, // cCollAddress, cBorrowAddress, exchangeAddress
-        bytes memory _callData
+        bytes memory _callData,
+        uint[2] memory _flashLoanData // amount, fee
     ) public payable {
-        // boost(_data, _addrData, _callData);
+        // draw max borrow
+        // swap max borrow + loanAmount
+        // add collateral
+        // borrow to repay loanAmount + loanFee
     }
 
 }
