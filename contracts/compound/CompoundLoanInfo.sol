a10817dc80c154b79d68e3ad10d0449d2f5c2040
Nenad Palinkasevic <nesa993@gmail.com>
2020-04-26 08:15:14 +0200

    Add token info getter

--- contracts/compound/CompoundLoanInfo.sol
+++ contracts/compound/CompoundLoanInfo.sol
@@ -3,17 +3,13 @@ pragma experimental ABIEncoderV2;
 
 import "./helpers/Exponential.sol";
 import "./helpers/ComptrollerInterface.sol";
+import "./CompoundSaverHelper.sol";
 
 contract CToken {
     function getAccountSnapshot(address account) external view returns (uint, uint, uint, uint);
 }
 
-contract CompoundOracle {
-    function getUnderlyingPrice(address cToken) external view returns (uint);
-}
-
-
-contract CompoundLoanInfo is Exponential {
+contract CompoundLoanInfo is Exponential, CompoundSaverHelper {
     // solhint-disable-next-line const-name-snakecase
     ComptrollerInterface public constant comp = ComptrollerInterface(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
 
@@ -28,6 +24,14 @@ contract CompoundLoanInfo is Exponential {
         uint[] collAmounts;
         uint[] borrowAmounts;
     }
+
+    struct TokenInfo {
+        address cTokenAddress;
+        address underlyingTokenAddress;
+        uint collateralFactor;
+        uint price;
+    }
+
 
     function getRatio(address _user) public view returns (uint) {
         // For each asset the account is in
@@ -68,7 +72,7 @@ contract CompoundLoanInfo is Exponential {
 
     function getPrices(address[] memory _cTokens) public view returns (uint[] memory prices) {
         prices = new uint[](_cTokens.length);
-        
+
         for (uint i = 0; i < _cTokens.length; ++i) {
             prices[i] = oracle.getUnderlyingPrice(_cTokens[i]);
         }
@@ -76,16 +80,16 @@ contract CompoundLoanInfo is Exponential {
 
     function getCollFactors(address[] memory _cTokens) public view returns (uint[] memory collFactors) {
         collFactors = new uint[](_cTokens.length);
-        
+
         for (uint i = 0; i < _cTokens.length; ++i) {
             (, collFactors[i]) = comp.markets(_cTokens[i]);
         }
     }
 
     function getLoanData(address _user) public view returns (LoanData memory data) {
-        
+
         address[] memory assets = comp.getAssetsIn(_user);
-        
+
         data = LoanData({
             user: _user,
             ratio: 0,
@@ -132,7 +136,7 @@ contract CompoundLoanInfo is Exponential {
                 borrowPos++;
             }
         }
-        
+
         if (sumBorrow == 0) return data;
 
         data.ratio = uint128((sumCollateral * 10**18) / sumBorrow);
@@ -141,7 +145,7 @@ contract CompoundLoanInfo is Exponential {
 
     function getLoanDataArr(address[] memory _users) public view returns (LoanData[] memory loans) {
         loans = new LoanData[](_users.length);
-        
+
         for (uint i = 0; i < _users.length; ++i) {
             loans[i] = getLoanData(_users[i]);
         }
@@ -149,9 +153,24 @@ contract CompoundLoanInfo is Exponential {
 
     function getRatios(address[] memory _users) public view returns (uint[] memory ratios) {
         ratios = new uint[](_users.length);
-        
+
         for (uint i = 0; i < _users.length; ++i) {
             ratios[i] = getRatio(_users[i]);
+        }
+    }
+
+    function getTokensInfo(address[] memory _cTokenAddresses) public returns(TokenInfo[] memory tokens) {
+        tokens = new TokenInfo[](_cTokenAddresses.length);
+
+        for (uint i = 0; i < _cTokenAddresses.length; ++i) {
+            (, uint collFactor) = comp.markets(_cTokenAddresses[i]);
+
+            tokens[i] = TokenInfo({
+                cTokenAddress: _cTokenAddresses[i],
+                underlyingTokenAddress: getUnderlyingAddr(_cTokenAddresses[i]),
+                collateralFactor: collFactor,
+                price: oracle.getUnderlyingPrice(_cTokenAddresses[i])
+            });
         }
     }
 }
