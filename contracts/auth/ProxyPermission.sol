85f0d4cf31311791e08527680bf10ca2e8c585f7
Nenad Palinkasevic <nesa993@gmail.com>
2020-04-26 08:39:08 +0200

    Document ProxyPermission

--- contracts/auth/ProxyPermission.sol
+++ contracts/auth/ProxyPermission.sol
@@ -4,9 +4,10 @@ import "../DS/DSGuard.sol";
 import "../DS/DSAuth.sol";
 
 contract ProxyPermission {
-
     address public constant FACTORY_ADDRESS = 0x5a15566417e6C1c9546523066500bDDBc53F88C7;
 
+    /// @notice Called in the context of DSProxy to authorize an address
+    /// @param _contractAddr Address which will be authorized
     function givePermission(address _contractAddr) internal {
         address currAuthority = address(DSAuth(address(this)).authority());
         DSGuard guard = DSGuard(currAuthority);
@@ -19,6 +20,8 @@ contract ProxyPermission {
         guard.permit(_contractAddr, address(this), bytes4(keccak256("execute(address,bytes)")));
     }
 
+    /// @notice Called in the context of DSProxy to remove authority of an address
+    /// @param _contractAddr Auth address which will be removed from authority list
     function removePermission(address _contractAddr) internal {
         address currAuthority = address(DSAuth(address(this)).authority());
         DSGuard guard = DSGuard(currAuthority);
