289366a4f8dcd3d8caeb958036468a72ecbabb3b
Nenad Palinkasevic <nesa993@gmail.com>
2020-05-25 15:36:59 +0200

    Disable warning

--- contracts/helpers/GasBurner.sol
+++ contracts/helpers/GasBurner.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.0;
 import "../interfaces/GasTokenInterface.sol";
 
 contract GasBurner {
+    // solhint-disable-next-line const-name-snakecase
     GasTokenInterface public constant gasToken = GasTokenInterface(0x0000000000b3F879cb30FE243b4Dfee438691c04);
 
     modifier burnGas(uint _amount) {
