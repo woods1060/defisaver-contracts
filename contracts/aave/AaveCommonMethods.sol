c3303dbc9c68a14cbc3e62618ee3e9d8805e4d7e
Nikola Klipa <klipa.nikola@gmail.com>
2020-06-11 14:10:23 +0200

    Recalculate max collateral if threshold changes

--- contracts/aave/AaveCommonMethods.sol
+++ contracts/aave/AaveCommonMethods.sol
@@ -11,7 +11,7 @@ contract AaveCommonMethods is DSMath {
 
 	address public constant ETH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address public constant AAVE_LENDING_POOL_ADDRESSES = 0x24a42fD28C976A61Df5D00D0599C34c4f90748c8;
-    uint public constant NINETY_NINE_PERCENT_WEI = 990000000000000000;
+    uint public constant NINETY_NINE_PERCENT_WEI = 999900000000000000;
 
     /// @param _collateralAddress underlying token address
     /// @param _user users address
@@ -19,17 +19,41 @@ contract AaveCommonMethods is DSMath {
         address lendingPoolAddress = ILendingPoolAddressesProvider(AAVE_LENDING_POOL_ADDRESSES).getLendingPool();
         address priceOracleAddress = ILendingPoolAddressesProvider(AAVE_LENDING_POOL_ADDRESSES).getPriceOracle();
 
+        // fetch all needed data
         (,uint256 totalCollateralETH, uint256 totalBorrowsETH,,,uint256 currentLiquidationThreshold,,) = ILendingPool(lendingPoolAddress).getUserAccountData(_user);
-		
-		// TODO: fix this, if more than one collateral, currentLiquidationThreshold changes
-        uint256 maxCollateralEth = div(sub(mul(currentLiquidationThreshold, totalCollateralETH), mul(totalBorrowsETH, 100)), currentLiquidationThreshold);
-        (uint256 userTokenCollateral,,,,,,,,, ) = ILendingPool(lendingPoolAddress).getUserReserveData(_collateralAddress, _user);
-
+        (,uint256 tokenLiquidationThreshold,,,,,,) = ILendingPool(lendingPoolAddress).getReserveConfigurationData(_collateralAddress);
         uint256 collateralPrice = IPriceOracleGetterAave(priceOracleAddress).getAssetPrice(_collateralAddress);
-        uint256 tokenAmount = wmul(wdiv(maxCollateralEth, collateralPrice), NINETY_NINE_PERCENT_WEI);
+		(uint256 userTokenBalance,,,,,,,,, ) = ILendingPool(lendingPoolAddress).getUserReserveData(_collateralAddress, _user);
+        uint256 userTokenBalanceEth = wmul(userTokenBalance, collateralPrice);
+		
+		// if borrow is 0, return whole user balance
+        if (totalBorrowsETH == 0) {
+        	return userTokenBalance;
+        }
 
-        /// @dev final amount can't be higher than users token balance
-		return tokenAmount > userTokenCollateral ? userTokenCollateral : tokenAmount;
+        uint256 maxCollateralEth = div(sub(mul(currentLiquidationThreshold, totalCollateralETH), mul(totalBorrowsETH, 100)), currentLiquidationThreshold);
+		/// @dev final amount can't be higher than users token balance
+        maxCollateralEth = maxCollateralEth > userTokenBalanceEth ? userTokenBalanceEth : maxCollateralEth;
+
+        // might happen due to wmul precision
+        if (maxCollateralEth >= totalCollateralETH) {
+        	return totalCollateralETH;
+        }
+
+        // TODO: name variable
+        // get sum of all other reserves multiplied with their liquidation thresholds by reversing formula
+        uint256 a = sub(wmul(currentLiquidationThreshold, totalCollateralETH), wmul(tokenLiquidationThreshold, userTokenBalanceEth));
+        // add new collateral amount multiplied by its threshold, and then divide with new total collateral
+        uint256 newLiquidationThreshold = wdiv(add(a, wmul(sub(userTokenBalanceEth, maxCollateralEth), tokenLiquidationThreshold)), sub(totalCollateralETH, maxCollateralEth));
+
+        // if new threshold is lower than first one, calculate new max collateral with newLiquidationThreshold
+        if (newLiquidationThreshold < currentLiquidationThreshold) {
+        	maxCollateralEth = div(sub(mul(newLiquidationThreshold, totalCollateralETH), mul(totalBorrowsETH, 100)), newLiquidationThreshold);
+        	maxCollateralEth = maxCollateralEth > userTokenBalanceEth ? userTokenBalanceEth : maxCollateralEth;
+        }
+
+        
+		return wmul(wdiv(maxCollateralEth, collateralPrice), NINETY_NINE_PERCENT_WEI);
 	}
 
 	/// @param _borrowAddress underlying token address
