f67b79dce29e3ba2389555f448b7f909e3bf24b7
nklipa13 <klipa.nikola@gmail.com>
2020-06-12 10:44:05 +0200

    Add Aave boost and repay

--- contracts/aave/AaveCommonMethods.sol
+++ contracts/aave/AaveCommonMethods.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.6.0;
 
 import "../DS/DSMath.sol";
+import "../DS/DSProxy.sol";
+import "../mcd/Discount.sol";
 import "../interfaces/ERC20.sol";
 import "../interfaces/IAToken.sol";
 import "../interfaces/ILendingPool.sol";
@@ -8,10 +10,14 @@ import "../interfaces/ILendingPoolAddressesProvider.sol";
 import "../interfaces/IPriceOracleGetterAave.sol";
 
 contract AaveCommonMethods is DSMath {
+    address payable public constant WALLET_ADDR = 0x322d58b9E75a6918f7e7849AEe0fF09369977e08;
+    address public constant DISCOUNT_ADDR = 0x1b14E8D511c9A4395425314f849bD737BAF8208F;
+    uint public constant SERVICE_FEE = 400; // 0.25% Fee
 
 	address public constant ETH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address public constant AAVE_LENDING_POOL_ADDRESSES = 0x24a42fD28C976A61Df5D00D0599C34c4f90748c8;
     uint public constant NINETY_NINE_PERCENT_WEI = 999900000000000000;
+    uint16 public constant AAVE_REFERRAL_CODE = 64;
 
     /// @param _collateralAddress underlying token address
     /// @param _user users address
@@ -69,4 +75,55 @@ contract AaveCommonMethods is DSMath {
 
 		return wmul(wdiv(availableBorrowsETH, borrowPrice), NINETY_NINE_PERCENT_WEI);
 	}
+
+    /// @notice Calculates the fee amount
+    /// @param _amount Amount that is converted
+    /// @param _user Actuall user addr not DSProxy
+    /// @param _gasCost Ether amount of gas we are spending for tx
+    /// @param _tokenAddr token addr. of token we are getting for the fee
+    /// @return feeAmount The amount we took for the fee
+    function getFee(uint _amount, address _user, uint _gasCost, address _tokenAddr) internal returns (uint feeAmount) {
+        address priceOracleAddress = ILendingPoolAddressesProvider(AAVE_LENDING_POOL_ADDRESSES).getPriceOracle();
+        uint fee = SERVICE_FEE;
+
+        if (Discount(DISCOUNT_ADDR).isCustomFeeSet(_user)) {
+            fee = Discount(DISCOUNT_ADDR).getCustomServiceFee(_user);
+        }
+
+        feeAmount = (fee == 0) ? 0 : (_amount / fee);
+
+        if (_gasCost != 0) {
+            uint256 price = IPriceOracleGetterAave(priceOracleAddress).getAssetPrice(_tokenAddr);
+            _gasCost = wmul(_gasCost, price);
+
+            feeAmount = add(feeAmount, _gasCost);
+        }
+
+        // fee can't go over 20% of the whole amount
+        if (feeAmount > (_amount / 5)) {
+            feeAmount = _amount / 5;
+        }
+
+        if (_tokenAddr == ETH_ADDR) {
+            WALLET_ADDR.transfer(feeAmount);
+        } else {
+            ERC20(_tokenAddr).transfer(WALLET_ADDR, feeAmount);
+        }
+    }
+
+    /// @notice Returns the owner of the DSProxy that called the contract
+    function getUserAddress() internal view returns (address) {
+        DSProxy proxy = DSProxy(payable(address(this)));
+
+        return proxy.owner();
+    }
+
+    /// @notice Approves token contract to pull underlying tokens from the DSProxy
+    /// @param _tokenAddr Token we are trying to approve
+    /// @param _caller Address which will gain the approval
+    function approveToken(address _tokenAddr, address _caller) internal {
+        if (_tokenAddr != ETH_ADDR) {
+            ERC20(_tokenAddr).approve(_caller, uint256(-1));
+        }
+    }
 }
--- contracts/aave/AaveSaverProxy.sol
+++ contracts/aave/AaveSaverProxy.sol
@@ -1,13 +1,77 @@
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
+import "./AaveCommonMethods.sol";
 import "../utils/GasBurner.sol";
 import "../interfaces/ERC20.sol";
 import "../interfaces/IAToken.sol";
 import "../interfaces/ILendingPool.sol";
 import "../exchange/SaverExchangeCore.sol";
-import "./AaveCommonMethods.sol";
+import "../loggers/DefisaverLogger.sol";
 
 contract AaveSaverProxy is GasBurner, SaverExchangeCore, AaveCommonMethods {
 
-	
+	// TODO: change address
+	address public constant DEFISAVER_LOGGER = 0x3DD0CDf5fFA28C6847B4B276e2fD256046a44bb7;
+
+	function repay(ExchangeData memory _data, uint _gasCost) public payable burnGas(0) {
+
+		address lendingPoolCore = ILendingPoolAddressesProvider(AAVE_LENDING_POOL_ADDRESSES).getLendingPoolCore();
+		address lendingPool = ILendingPoolAddressesProvider(AAVE_LENDING_POOL_ADDRESSES).getLendingPool();
+		address payable user = payable(getUserAddress());
+
+		uint256 maxCollateral = getMaxCollateral(_data.srcAddr, address(this));
+		// don't swap more than maxCollateral
+		_data.srcAmount = _data.srcAmount > maxCollateral ? maxCollateral : _data.srcAmount;
+
+		// redeem collateral
+		address aTokenCollateral = ILendingPool(lendingPoolCore).getReserveATokenAddress(_data.srcAddr);
+		IAToken(aTokenCollateral).redeem(_data.srcAmount);
+
+		// swap
+		(, uint256 destAmount) = _sell(_data);
+
+		destAmount -= getFee(destAmount, user, _gasCost, _data.destAddr);
+
+		// payback
+		if (_data.destAddr == ETH_ADDR) {
+			ILendingPool(lendingPool).repay{value: destAmount}(_data.destAddr, destAmount, payable(address(this)));
+		} else {
+			approveToken(_data.destAddr, lendingPoolCore);
+			ILendingPool(lendingPool).repay(_data.destAddr, destAmount, payable(address(this)));
+		}
+		
+		// returning to msg.sender as it is the address that actually sent 0x fee
+		msg.sender.transfer(address(this).balance);
+
+		DefisaverLogger(DEFISAVER_LOGGER).Log(address(this), msg.sender, "AaveRepay", abi.encode(_data.srcAddr, _data.destAddr, _data.srcAmount, destAmount));
+	}
+
+	function boost(ExchangeData memory _data, uint _gasCost) public payable burnGas(0) {
+		address lendingPoolCore = ILendingPoolAddressesProvider(AAVE_LENDING_POOL_ADDRESSES).getLendingPoolCore();
+		address lendingPool = ILendingPoolAddressesProvider(AAVE_LENDING_POOL_ADDRESSES).getLendingPool();
+		address payable user = payable(getUserAddress());	
+
+		uint256 maxBorrow = getMaxBorrow(_data.srcAddr, address(this));
+		_data.srcAmount = _data.srcAmount > maxBorrow ? maxBorrow : _data.srcAmount;
+
+		// borrow amount
+		ILendingPool(lendingPool).borrow(_data.srcAddr, _data.srcAmount, 1, AAVE_REFERRAL_CODE);
+		_data.srcAmount -= getFee(_data.srcAmount, user, _gasCost, _data.srcAddr);
+
+		// swap
+		(, uint256 destAmount) = _sell(_data);
+
+		if (_data.destAddr == ETH_ADDR) {
+			ILendingPool(lendingPool).deposit{value: destAmount}(_data.destAddr, destAmount, AAVE_REFERRAL_CODE);
+		} else {
+			approveToken(_data.destAddr, lendingPoolCore);
+			ILendingPool(lendingPool).deposit(_data.destAddr, destAmount, AAVE_REFERRAL_CODE);
+		}
+
+		// returning to msg.sender as it is the address that actually sent 0x fee
+		msg.sender.transfer(address(this).balance);
+
+		DefisaverLogger(DEFISAVER_LOGGER).Log(address(this), msg.sender, "AaveBoost", abi.encode(_data.srcAddr, _data.destAddr, _data.srcAmount, destAmount));
+	}
 }
