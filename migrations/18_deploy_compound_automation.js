a48b723677ef0793f7494d111cb993cec53c5b3a
nklipa13 <klipa.nikola@gmail.com>
2020-07-06 14:31:37 +0200

    Deploy new Compound automation

--- migrations/18_deploy_compound_automation.js
+++ migrations/18_deploy_compound_automation.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 module.exports = function(deployer, network, accounts) {
     let deployAgain = (process.env.DEPLOY_AGAIN === 'true') ? true : false;
 
-    const compoundFlashLoanTakerAddress = '0x1c214eCB456D0D4403984E5593BE7992CF8B9eB8';
+    const compoundFlashLoanTakerAddress = '0xC39c67C99E1053cAF566C0Ec86c090991E7Ce81a';
     const changePeriod = 0;
 
     deployer.then(async () => {
@@ -24,15 +24,16 @@ module.exports = function(deployer, network, accounts) {
     	const compoundMonitorProxyAddress = '0xB1cF8DE8e791E4Ed1Bd86c03E2fc1f14389Cb10a'; //(await CompoundMonitorProxy.deployed()).address;
     	const subscriptionsAddress = '0x52015EFFD577E08f498a0CCc11905925D58D6207'; //(await CompoundSubscriptions.deployed()).address;
 
-        await deployer.deploy(CompoundSubscriptionsProxy, {gas: 700000, overwrite: deployAgain});
-        await deployer.deploy(CompoundMonitor, compoundMonitorProxyAddress, subscriptionsAddress, compoundFlashLoanTakerAddress, {gas: 4000000, overwrite: deployAgain});
-    	const compoundMonitorAddress = (await CompoundMonitor.deployed()).address;
+        // await deployer.deploy(CompoundSubscriptionsProxy, {gas: 700000, overwrite: deployAgain});
+        // await deployer.deploy(CompoundMonitor, compoundMonitorProxyAddress, subscriptionsAddress, compoundFlashLoanTakerAddress, {gas: 4000000, overwrite: deployAgain});
+    	const compoundMonitorAddress = '0x9DDc4A171410Ee4C1594d67d7Eb6E222A50c8A2D'; //(await CompoundMonitor.deployed()).address;
+        const subscriptionsProxyAddress = '0x0ebaCEC7645b488eE477eee35902A42482E548A5';
 
-    	const compoundMonitorProxy = await CompoundMonitorProxy.at(compoundMonitorProxyAddress);
-    	await compoundMonitorProxy.setMonitor(compoundMonitorAddress);
+    	// const compoundMonitorProxy = await CompoundMonitorProxy.at(compoundMonitorProxyAddress);
+    	// await compoundMonitorProxy.setMonitor(compoundMonitorAddress);
 
-    	const monitor = await CompoundMonitor.at(compoundMonitorAddress);
-    	await monitor.addCaller('0x6c259ea1fCa0D1883e3FFFdDeb8a0719E1D7265f');
+    	// const monitor = await CompoundMonitor.at(compoundMonitorAddress);
+    	// await monitor.addCaller('0x6c259ea1fCa0D1883e3FFFdDeb8a0719E1D7265f');
     });
 };
 
--- truffle.js
+++ truffle.js
@@ -19,7 +19,7 @@ module.exports = {
             return new HDWalletProvider(mnemonic, process.env.ALCHEMY_NODE);
         },
         network_id: '1',
-        gasPrice: 41100000000, // 8.1 gwei
+        gasPrice: 38100000000, // 8.1 gwei
         skipDryRun: true,
     },
     kovan: {
