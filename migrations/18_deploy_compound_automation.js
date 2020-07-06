b803efef65706307b453fbab8c2e3e13a86d00e9
nklipa13 <klipa.nikola@gmail.com>
2020-07-06 13:49:21 +0200

    Add deployed addresses for Subscriptions and MonitorProxy

--- migrations/18_deploy_compound_automation.js
+++ migrations/18_deploy_compound_automation.js
@@ -14,25 +14,25 @@ module.exports = function(deployer, network, accounts) {
     deployer.then(async () => {
 
     	// -------------------- first deploy this -----------------------------
-    	await deployer.deploy(CompoundSubscriptions, {gas: 4000000, overwrite: deployAgain});
-    	await deployer.deploy(CompoundMonitorProxy, changePeriod, {gas: 2000000, overwrite: deployAgain});
+        // await deployer.deploy(CompoundMonitorProxy, changePeriod, {gas: 2000000, overwrite: deployAgain});
+    	// await deployer.deploy(CompoundSubscriptions, {gas: 4000000, overwrite: deployAgain});
 
 
     	// ------------------- second part ----------------------
     	// check SubscriptionProxy.sol before running this!
     	// set these two addresses manually 
-    	// const compoundMonitorProxyAddress = (await CompoundMonitorProxy.deployed()).address;
-    	// const subscriptionsAddress = (await CompoundSubscriptions.deployed()).address;
+    	const compoundMonitorProxyAddress = '0xB1cF8DE8e791E4Ed1Bd86c03E2fc1f14389Cb10a'; //(await CompoundMonitorProxy.deployed()).address;
+    	const subscriptionsAddress = '0x52015EFFD577E08f498a0CCc11905925D58D6207'; //(await CompoundSubscriptions.deployed()).address;
 
-     //    await deployer.deploy(CompoundSubscriptionsProxy, {gas: 700000, overwrite: deployAgain});
-     //    await deployer.deploy(CompoundMonitor, compoundMonitorProxyAddress, subscriptionsAddress, compoundFlashLoanTakerAddress, {gas: 4000000, overwrite: deployAgain});
-    	// const compoundMonitorAddress = (await CompoundMonitor.deployed()).address;
+        await deployer.deploy(CompoundSubscriptionsProxy, {gas: 700000, overwrite: deployAgain});
+        await deployer.deploy(CompoundMonitor, compoundMonitorProxyAddress, subscriptionsAddress, compoundFlashLoanTakerAddress, {gas: 4000000, overwrite: deployAgain});
+    	const compoundMonitorAddress = (await CompoundMonitor.deployed()).address;
 
-    	// const compoundMonitorProxy = await CompoundMonitorProxy.at(compoundMonitorProxyAddress);
-    	// await compoundMonitorProxy.setMonitor(compoundMonitorAddress);
+    	const compoundMonitorProxy = await CompoundMonitorProxy.at(compoundMonitorProxyAddress);
+    	await compoundMonitorProxy.setMonitor(compoundMonitorAddress);
 
-    	// const monitor = await CompoundMonitor.at(compoundMonitorAddress);
-    	// await monitor.addCaller('0x6c259ea1fCa0D1883e3FFFdDeb8a0719E1D7265f');
+    	const monitor = await CompoundMonitor.at(compoundMonitorAddress);
+    	await monitor.addCaller('0x6c259ea1fCa0D1883e3FFFdDeb8a0719E1D7265f');
     });
 };
 
