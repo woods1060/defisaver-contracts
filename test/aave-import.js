f3135860ce0b26d9e41d28a725d28c158bf7daf5
nklipa13 <klipa.nikola@gmail.com>
2020-08-06 17:05:03 +0200

    Add test for migration

--- test/aave-import.js
+++ test/aave-import.js
@@ -15,10 +15,11 @@ const ETH_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 const AETH_ADDRESS = '0x3a3a65aab0dd2a17e3f1947ba16138cd37d08c04';
 const ADAI_ADDRESS = '0xfc1e690f61efd961294b3e1ce3313fbd8aa4f85d';
 const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+const usdc = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
 
-const aaveBasicProxyAddr = "0x2B816BFeD57bD7f36280CAaD292220dCA5004a6a";
-const aaveImportTakerAddr = "0x4bf749ec68270027C5910220CEAB30Cc284c7BA2";
-const aaveImportAddr = '0x86072CbFF48dA3C1F01824a6761A03F105BCC697';
+const aaveBasicProxyAddr = "0x9D266997bc73B27d4302E711b55FD78B5278e1De";
+const aaveImportTakerAddr = "0x6b619F5eC6703eE1Ee85aCF58158561edF65aE97";
+const aaveImportAddr = '0x207754BD0044BAd0C7021ca06643C26d59d8AD8f';
 
 const makerVersion = "1.0.6";
 
@@ -35,16 +36,19 @@ describe("AaveImport", () => {
         makerAddresses = await fetchMakerAddresses(makerVersion);
 
         registry = await ProxyRegistryInterface.at("0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4");
+        web3registry = new web3.eth.Contract(ProxyRegistryInterface.abi, "0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4");
         aaveBasicProxy = await AaveBasicProxy.at(aaveBasicProxyAddr);
         aaveImportTaker = new web3.eth.Contract(AaveImportTaker.abi, aaveImportTakerAddr);
 
-        const proxyInfo = await getProxy(registry, accounts[0]);
-        proxy = proxyInfo.proxy;
-        proxyAddr = proxyInfo.proxyAddr;
+        // const proxyInfo = await getProxy(registry, accounts[0]);
+        proxyAddr = await web3registry.methods.proxies(accounts[0]).call();
+        proxy = new web3.eth.Contract(DSProxy.abi, proxyAddr);
+
+        console.log(proxyAddr);
     });
 
     it('...should import loan from acc to proxy', async () => {
-        const userEthDeposited = await getBalance(web3, accounts[0], AETH_ADDRESS);
+        const userEthDeposited = await getBalance(web3, proxyAddr, AETH_ADDRESS);
         console.log(userEthDeposited.toString());
 
         await fundIfNeeded(web3, accounts[0], aaveImportAddr, minBal='0.1', addBal='0.1');
@@ -52,13 +56,13 @@ describe("AaveImport", () => {
         const wethBalance = await getBalance(web3, aaveImportAddr, WETH_ADDRESS);
         console.log('weth', wethBalance.toString());
 
-        await approve(web3, AETH_ADDRESS, accounts[0], proxyAddr, web3.utils.toWei('100000'));
         console.log('approved');
+        await approve(web3, AETH_ADDRESS, accounts[0], aaveImportAddr, web3.utils.toWei('100000'));
 
         const data = web3.eth.abi.encodeFunctionCall(getAbiFunction(AaveImportTaker, 'importLoan'),
-          [ETH_ADDRESS, makerAddresses["MCD_DAI"], web3.utils.toWei('10')]);
-        await proxy.methods['execute(address,bytes)'](aaveImportTakerAddr, data, {
-            from: accounts[0] });
+          [ETH_ADDRESS, usdc, web3.utils.toWei('10')]);
+        const tx = await proxy.methods['execute(address,bytes)'](aaveImportTakerAddr, data).send({
+           from: accounts[0], gasPrice: 1000000000, gas: 3500000 });
 
         console.log('imported');
 
