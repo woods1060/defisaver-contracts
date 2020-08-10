d65537cd7b2cc86170f0536bd2ab458b75675dc4
nklipa13 <klipa.nikola@gmail.com>
2020-08-10 17:32:32 +0200

    Change CompoundSaverProxy to support SaverExchangeCore

--- contracts/compound/saver/CompoundSaverProxy.sol
+++ contracts/compound/saver/CompoundSaverProxy.sol
@@ -1,31 +1,32 @@
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
-import "../../mcd/saver_proxy/ExchangeHelper.sol";
+import "../../exchange/SaverExchangeCore.sol";
 import "../../loggers/DefisaverLogger.sol";
 import "../helpers/CompoundSaverHelper.sol";
 
 /// @title Contract that implements repay/boost functionality
-contract CompoundSaverProxy is CompoundSaverHelper, ExchangeHelper {
+contract CompoundSaverProxy is CompoundSaverHelper, SaverExchangeCore {
 
     address public constant DEFISAVER_LOGGER = 0x5c55B921f590a89C1Ebe84dF170E655a82b62126;
 
     /// @notice Withdraws collateral, converts to borrowed token and repays debt
     /// @dev Called through the DSProxy
-    /// @param _data Amount and exchange data for the repay [amount, minPrice, exchangeType, gasCost, 0xPrice]
-    /// @param _addrData Coll/Debt addresses [cCollAddress, cBorrowAddress, exchangeAddress]
-    /// @param _callData 0x calldata info
+    /// @param _exData Exchange data
+    /// @param _addrData Coll/Debt addresses [cCollAddress, cBorrowAddress]
+    /// @param _gasCost Gas cost for specific transaction
     function repay(
-        uint[5] memory _data, // amount, minPrice, exchangeType, gasCost, 0xPrice
-        address[3] memory _addrData, // cCollAddress, cBorrowAddress, exchangeAddress
-        bytes memory _callData
+        ExchangeData memory _exData,
+        address[2] memory _addrData, // cCollAddress, cBorrowAddress
+        uint256 _gasCost
     ) public payable {
         enterMarket(_addrData[0], _addrData[1]);
 
-        address payable user = address(uint160(getUserAddress()));
+        address payable user = payable(getUserAddress());
 
         uint maxColl = getMaxCollateral(_addrData[0], address(this));
 
-        uint collAmount = (_data[0] > maxColl) ? maxColl : _data[0];
+        uint collAmount = (_exData.srcAmount > maxColl) ? maxColl : _exData.srcAmount;
 
         require(CTokenInterface(_addrData[0]).redeemUnderlying(collAmount) == 0);
 
@@ -35,18 +36,11 @@ contract CompoundSaverProxy is CompoundSaverHelper, ExchangeHelper {
         uint swapAmount = 0;
 
         if (collToken != borrowToken) {
-            swapAmount = swap(
-                [collAmount, _data[1], _data[2], _data[4]], // collAmount, minPrice, exchangeType, 0xPrice
-                collToken,
-                borrowToken,
-                _addrData[2],
-                _callData
-            );
-
-            swapAmount -= getFee(swapAmount, user, _data[3], _addrData[1]);
+            (, swapAmount) = _sell(_exData);
+            swapAmount -= getFee(swapAmount, user, _gasCost, _addrData[1]);
         } else {
             swapAmount = collAmount;
-            swapAmount -= getGasCost(swapAmount, _data[3], _addrData[1]);
+            swapAmount -= getGasCost(swapAmount, _gasCost, _addrData[1]);
         }
 
         paybackDebt(swapAmount, _addrData[1], borrowToken, user);
@@ -55,25 +49,25 @@ contract CompoundSaverProxy is CompoundSaverHelper, ExchangeHelper {
         user.transfer(address(this).balance);
 
         // log amount, collToken, borrowToken
-        DefisaverLogger(DEFISAVER_LOGGER).Log(address(this), msg.sender, "CompoundRepay", abi.encode(_data[0], swapAmount, collToken, borrowToken));
+        DefisaverLogger(DEFISAVER_LOGGER).Log(address(this), msg.sender, "CompoundRepay", abi.encode(_exData.srcAmount, swapAmount, collToken, borrowToken));
     }
 
     /// @notice Borrows token, converts to collateral, and adds to position
     /// @dev Called through the DSProxy
-    /// @param _data Amount and exchange data for the boost [amount, minPrice, exchangeType, gasCost, 0xPrice]
+    /// @param _exData Exchange data
     /// @param _addrData Coll/Debt addresses [cCollAddress, cBorrowAddress, exchangeAddress]
-    /// @param _callData 0x calldata info
+    /// @param _gasCost Gas cost for specific transaction
     function boost(
-        uint[5] memory _data, // amount, minPrice, exchangeType, gasCost, 0xPrice
+        ExchangeData memory _exData,
         address[3] memory _addrData, // cCollAddress, cBorrowAddress, exchangeAddress
-        bytes memory _callData
+        uint256 _gasCost
     ) public payable {
         enterMarket(_addrData[0], _addrData[1]);
 
         address payable user = address(uint160(getUserAddress()));
 
         uint maxBorrow = getMaxBorrow(_addrData[1], address(this));
-        uint borrowAmount = (_data[0] > maxBorrow) ? maxBorrow : _data[0];
+        uint borrowAmount = (_exData.srcAmount > maxBorrow) ? maxBorrow : _exData.srcAmount;
 
         require(CTokenInterface(_addrData[1]).borrow(borrowAmount) == 0);
 
@@ -83,18 +77,12 @@ contract CompoundSaverProxy is CompoundSaverHelper, ExchangeHelper {
         uint swapAmount = 0;
 
         if (collToken != borrowToken) {
-            borrowAmount -= getFee(borrowAmount, user, _data[3], _addrData[1]);
+            borrowAmount -= getFee(borrowAmount, user, _gasCost, _addrData[1]);
 
-            swapAmount = swap(
-                [borrowAmount, _data[1], _data[2], _data[4]], // collAmount, minPrice, exchangeType, 0xPrice
-                borrowToken,
-                collToken,
-                _addrData[2],
-                _callData
-            );
+            (,swapAmount) = _sell(_exData);
         } else {
             swapAmount = borrowAmount;
-            swapAmount -= getGasCost(swapAmount, _data[3], _addrData[1]);
+            swapAmount -= getGasCost(swapAmount, _gasCost, _addrData[1]);
         }
 
         approveCToken(collToken, _addrData[0]);
@@ -109,7 +97,7 @@ contract CompoundSaverProxy is CompoundSaverHelper, ExchangeHelper {
         user.transfer(address(this).balance);
 
         // log amount, collToken, borrowToken
-        DefisaverLogger(DEFISAVER_LOGGER).Log(address(this), msg.sender, "CompoundBoost", abi.encode(_data[0], swapAmount, collToken, borrowToken));
+        DefisaverLogger(DEFISAVER_LOGGER).Log(address(this), msg.sender, "CompoundBoost", abi.encode(_exData.srcAmount, swapAmount, collToken, borrowToken));
     }
 
 }
