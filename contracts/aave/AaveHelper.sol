137268acd80fa311c7aef709137dc63c154dabc0
Nikola Klipa <klipa.nikola@gmail.com>
2020-10-20 20:29:31 +0200

    Fix aave gas cost calculation

--- contracts/aave/AaveHelper.sol
+++ contracts/aave/AaveHelper.sol
@@ -124,7 +124,7 @@ contract AaveHelper is DSMath {
         if (_gasCost != 0) {
             uint256 price = IPriceOracleGetterAave(priceOracleAddress).getAssetPrice(_tokenAddr);
 
-            _gasCost = wmul(_gasCost, price) / (10 ** (18 - _getDecimals(_tokenAddr)));
+            _gasCost = wdiv(_gasCost, price) / (10 ** (18 - _getDecimals(_tokenAddr)));
 
             feeAmount = add(feeAmount, _gasCost);
         }
