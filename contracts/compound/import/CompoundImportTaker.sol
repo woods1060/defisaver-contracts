9422fee6b2943080cb3ae97c11400867b0b0b9a8
Nenad Palinkasevic <nesa993@gmail.com>
2020-10-28 09:29:03 +0100

    Fix approve bug in Compound import

--- contracts/compound/import/CompoundImportTaker.sol
+++ contracts/compound/import/CompoundImportTaker.sol
@@ -15,7 +15,7 @@ contract CompoundImportTaker is CompoundSaverHelper, ProxyPermission, GasBurner 
 
     ILendingPool public constant lendingPool = ILendingPool(0x398eC7346DcD622eDc5ae82352F02bE94C62d119);
 
-    address payable public constant COMPOUND_IMPORT_FLASH_LOAN = 0x0a9238e14d5A20CDb03811B12D1984587C3CE9a0;
+    address payable public constant COMPOUND_IMPORT_FLASH_LOAN = 0xaf9f8781A4c39Ce2122019fC05F22e3a662B0A32;
     address public constant PROXY_REGISTRY_ADDRESS = 0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4;
 
     DefisaverLogger public constant logger = DefisaverLogger(0x5c55B921f590a89C1Ebe84dF170E655a82b62126);
