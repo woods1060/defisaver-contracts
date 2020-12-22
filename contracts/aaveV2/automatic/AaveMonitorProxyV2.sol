344a63a4a2adc61180f48aa3bac1404091c6382e
Nikola Klipa <klipa.nikola@gmail.com>
2020-12-22 11:09:37 +0800

    Monitor proxy cleanup

--- contracts/aaveV2/automatic/AaveMonitorProxyV2.sol
+++ contracts/aaveV2/automatic/AaveMonitorProxyV2.sol
@@ -17,18 +17,11 @@ contract AaveMonitorProxyV2 is AdminAuth {
     address public lastMonitor;
     uint public changeRequestedTimestamp;
 
-    mapping(address => bool) public allowed;
-
     event MonitorChangeInitiated(address oldMonitor, address newMonitor);
     event MonitorChangeCanceled();
     event MonitorChangeFinished(address monitor);
     event MonitorChangeReverted(address monitor);
 
-    // if someone who is allowed become malicious, owner can't be changed
-    modifier onlyAllowed() {
-        require(allowed[msg.sender] || msg.sender == owner);
-        _;
-    }
 
     modifier onlyMonitor() {
         require (msg.sender == monitor);
@@ -53,17 +46,17 @@ contract AaveMonitorProxyV2 is AdminAuth {
         }
     }
 
-    /// @notice Allowed users are able to set Monitor contract without any waiting period first time
+    /// @notice Owner is able to set Monitor contract without any waiting period first time
     /// @param _monitor Address of Monitor contract
-    function setMonitor(address _monitor) public onlyAllowed {
+    function setMonitor(address _monitor) public onlyOwner {
         require(monitor == address(0));
         monitor = _monitor;
     }
 
-    /// @notice Allowed users are able to start procedure for changing monitor
+    /// @notice Owner is able to start procedure for changing monitor
     /// @dev after CHANGE_PERIOD needs to call confirmNewMonitor to actually make a change
     /// @param _newMonitor address of new monitor
-    function changeMonitor(address _newMonitor) public onlyAllowed {
+    function changeMonitor(address _newMonitor) public onlyOwner {
         require(changeRequestedTimestamp == 0);
 
         changeRequestedTimestamp = now;
@@ -73,8 +66,8 @@ contract AaveMonitorProxyV2 is AdminAuth {
         emit MonitorChangeInitiated(lastMonitor, newMonitor);
     }
 
-    /// @notice At any point allowed users are able to cancel monitor change
-    function cancelMonitorChange() public onlyAllowed {
+    /// @notice At any point owner is able to cancel monitor change
+    function cancelMonitorChange() public onlyOwner {
         require(changeRequestedTimestamp > 0);
 
         changeRequestedTimestamp = 0;
@@ -84,7 +77,7 @@ contract AaveMonitorProxyV2 is AdminAuth {
     }
 
     /// @notice Anyone is able to confirm new monitor after CHANGE_PERIOD if process is started
-    function confirmNewMonitor() public onlyAllowed {
+    function confirmNewMonitor() public onlyOwner {
         require((changeRequestedTimestamp + CHANGE_PERIOD) < now);
         require(changeRequestedTimestamp != 0);
         require(newMonitor != address(0));
@@ -97,7 +90,7 @@ contract AaveMonitorProxyV2 is AdminAuth {
     }
 
     /// @notice Its possible to revert monitor to last used monitor
-    function revertMonitor() public onlyAllowed {
+    function revertMonitor() public onlyOwner {
         require(lastMonitor != address(0));
 
         monitor = lastMonitor;
@@ -105,36 +98,9 @@ contract AaveMonitorProxyV2 is AdminAuth {
         emit MonitorChangeReverted(monitor);
     }
 
+    function setChangePeriod(uint _periodInDays) public onlyOwner {
+        require(_periodInDays * 1 hours > CHANGE_PERIOD);
 
-    /// @notice Allowed users are able to add new allowed user
-    /// @param _user Address of user that will be allowed
-    function addAllowed(address _user) public onlyAllowed {
-        allowed[_user] = true;
-    }
-
-    /// @notice Allowed users are able to remove allowed user
-    /// @dev owner is always allowed even if someone tries to remove it from allowed mapping
-    /// @param _user Address of allowed user
-    function removeAllowed(address _user) public onlyAllowed {
-        allowed[_user] = false;
-    }
-
-    function setChangePeriod(uint _periodInDays) public onlyAllowed {
-        require(_periodInDays * 1 days > CHANGE_PERIOD);
-
-        CHANGE_PERIOD = _periodInDays * 1 days;
-    }
-
-    /// @notice In case something is left in contract, owner is able to withdraw it
-    /// @param _token address of token to withdraw balance
-    function withdrawToken(address _token) public onlyOwner {
-        uint balance = ERC20(_token).balanceOf(address(this));
-        ERC20(_token).safeTransfer(msg.sender, balance);
-    }
-
-    /// @notice In case something is left in contract, owner is able to withdraw it
-    function withdrawEth() public onlyOwner {
-        uint balance = address(this).balance;
-        msg.sender.transfer(balance);
+        CHANGE_PERIOD = _periodInDays * 1 hours;
     }
 }
