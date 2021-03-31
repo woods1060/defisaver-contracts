ffdb9a2eae9872993db201d85f4fffc5bc7f6c94
Nikola Klipa <klipa.nikola@gmail.com>
2021-03-31 15:40:52 +0800

    Redeploy aave monitor

--- deployers/aave-automation.js
+++ deployers/aave-automation.js
@@ -9,7 +9,9 @@ async function main() {
  	// const subscriptions = await deployContract("AaveSubscriptions");
  	// const monitorProxy = await deployContract("AaveMonitorProxy", 0);
 
- 	await deployContract("AaveMonitor", '0xfA560Dba3a8D0B197cA9505A2B98120DD89209AC', '0xe08ff7A2BADb634F0b581E675E6B3e583De086FC', "0x2D67F20cb905D50545dF90e6B9154F9ed8cd294c")
+
+ 	const aaveProxy = "0x2a3B273695A045EC263970e3C86c23800a0F04FC";
+ 	await deployContract("AaveMonitor", '0xfA560Dba3a8D0B197cA9505A2B98120DD89209AC', '0xe08ff7A2BADb634F0b581E675E6B3e583De086FC', aaveProxy);
 
  	// await deployContract("AaveSubscriptionsProxy");
 }
